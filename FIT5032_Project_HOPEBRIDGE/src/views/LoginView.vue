7338f95a9f5dc31345c11cf6484aaef6cdf1dcaf
jjimmyLin <iaminhi3124@gmail.com>
2024-09-02 17:47:31 +1000

    Logindone, change redirection to my account page after register/log in

--- FIT5032_Project_HOPEBRIDGE/src/views/LoginView.vue
+++ FIT5032_Project_HOPEBRIDGE/src/views/LoginView.vue
@@ -1,5 +1,80 @@
+<!--This page basically was from registerviewpage just to remain consistency of the look, and 
+functionised by changing from register to login, removed password valied as I believe a 
+registered user should know their pw and if theres validation will look like a "hint" to 
+the pw structure, instead when login with  :) hope it make sense-->
+<script setup>
+import { ref } from "vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const email = ref("");
+const password = ref("");
+const error = ref({
+    email: null,
+    password: null
+});
+const router = useRouter();
+
+const login = () => {
+    validateEmail(true);
+
+    if (!error.value.email) {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email.value, password.value)
+            .then(() => {
+                console.log("Login Successful!");
+                alert("Login Successful!");
+                router.push("/MyAccount");  // Redirect to MyAccount
+            })
+            .catch((e) => {
+                alert("Error occurred: \n" + e.message + " \nPlease input correct information");
+            });
+    }
+};
+
+const validateEmail = (blur) => {
+    if (!email.value) {
+        if (blur) error.value.email = 'Email is required.';
+    } else if (!email.value.includes('@')) {
+        if (blur) error.value.email = 'Email must contain an "@" character.';
+    } else if (!email.value.endsWith('.com')) {
+        if (blur) error.value.email = 'Email must end with ".com".';
+    } else {
+        error.value.email = null;
+    }
+};
+</script>
+
+
 <template>
     <h1>
-    Hello
+        😀 Please log in 😀
     </h1>
+
+    <div class="container col-md-6 col-sm-6">
+        <div>
+            <p>
+                <input type="text" v-model="email" @blur="validateEmail(true)" placeholder="Email" />
+            </p>
+            <p v-if="error.email" style="color: red;">{{ error.email }}</p>
+        </div>
+
+        <div>
+            <p>
+                <input type="password" v-model="password" placeholder="Password" />
+            </p>
+            <p v-if="error.password" style="color: red;">{{ error.password }}</p>
+        </div>
+    </div>
+
+    <button @click="login" class="btn btn-primary">Login</button>
+
+
 </template>
+
+<style scoped>
+.container {
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- FIT5032_Project_HOPEBRIDGE/src/views/RegisterView.vue
+++ FIT5032_Project_HOPEBRIDGE/src/views/RegisterView.vue
@@ -1,27 +1,28 @@
 <script setup>
 import { ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from "vue-router";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; //required firebase auth functions inorder to register an account
+import { useRouter } from "vue-router"; //for redirect purpose after register
 const email = ref("");
 const password = ref("");
-const errors = ref({
+const error = ref({
     email: null,
     password: null
-});
+}); 
 const router = useRouter();
-
 
 const register = () => {
     validateEmail(true);
     validatePassword(true); 
     //run these two method again before register ensures valid email and password to be uploaded
 
-    if (!errors.value.email && !errors.value.password) {
+    if (!error.value.email && !error.value.password) {
+        const auth = getAuth()
         createUserWithEmailAndPassword(getAuth(), email.value, password.value)
             .then((data) => {
                 console.log("Register Successful!")
+                console.log(auth.currentUser)
                 alert("Register Successful!");
-                router.push("/");
+                router.push("/MyAccount");// Redirect to MyAccount
             })
 
             .catch((e) => {
@@ -30,7 +31,7 @@ const register = () => {
     }
 };
 
-//part of below password validate was from NOMONASH-LIBRARY project
+//part of below password validate method was from NOMONASH-LIBRARY project
 const validatePassword = (blur) => {
     const minLength = 9
     const hasUppercase = /[A-Z]/.test(password.value)
@@ -39,28 +40,28 @@ const validatePassword = (blur) => {
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password.value)
 
     if (password.value.length < minLength) {
-        if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+        if (blur) error.value.password = `Password must be at least ${minLength} characters long.`
     } else if (!hasUppercase) {
-        if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+        if (blur) error.value.password = 'Password must contain at least one uppercase letter.'
     } else if (!hasLowercase) {
-        if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+        if (blur) error.value.password = 'Password must contain at least one lowercase letter.'
     } else if (!hasNumber) {
-        if (blur) errors.value.password = 'Password must contain at least one number.'
+        if (blur) error.value.password = 'Password must contain at least one number.'
     } else if (!hasSpecialChar) {
-        if (blur) errors.value.password = 'Password must contain at least one special character.'
+        if (blur) error.value.password = 'Password must contain at least one special character.'
     } else {
-        errors.value.password = null
+        error.value.password = null
     }
 }
 const validateEmail = (blur) => {
     if (!email.value) {
-        if (blur) errors.value.email = 'Email is required.';
+        if (blur) error.value.email = 'Email is required.';
     } else if (!email.value.includes('@')) {
-        if (blur) errors.value.email = 'Email must contain an "@" character.';
+        if (blur) error.value.email = 'Email must contain an "@" character.';
     } else if (!email.value.endsWith('.com')) {
-        if (blur) errors.value.email = 'Email must end with ".com".';
+        if (blur) error.value.email = 'Email must end with ".com".';
     } else {
-        errors.value.email = null;
+        error.value.email = null;
     }
 };
 </script>
@@ -75,14 +76,14 @@ const validateEmail = (blur) => {
             <p>
                 <input type="text" v-model="email" @blur="validateEmail(true)" placeholder="Email" />
             </p>
-            <p v-if="errors.email" style="color: red;">{{ errors.email }}</p>
+            <p v-if="error.email" style="color: red;">{{ error.email }}</p>
         </div>
 
         <div>
             <p>
                 <input type="password" v-model="password" @blur="validatePassword(true)" placeholder="Password" />
             </p>
-            <p v-if="errors.password" style="color: red;">{{ errors.password }}</p>
+            <p v-if="error.password" style="color: red;">{{ error.password }}</p>
         </div>
     </div>
 
